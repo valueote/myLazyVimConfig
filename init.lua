d36e6885ebd87f3a4ffb793d1368d45d91b39ae9
valueote <valueote@outlook.com>
2026-02-09 05:54:35 +0000

    add remote paste support

--- init.lua
+++ init.lua
@@ -1,2 +1,20 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local osc52 = require("vim.ui.clipboard.osc52")
+
+local function paste()
+  -- 不向终端请求系统剪贴板，直接返回 nvim 当前寄存器，避免 timeout
+  return { vim.fn.getreg('"', 1, true), vim.fn.getregtype('"') }
+end
+
+vim.g.clipboard = {
+  name = "OSC52",
+  copy = {
+    ["+"] = osc52.copy("+"),
+    ["*"] = osc52.copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
